b602d6d8d9b4f9ffe65bcfbbb5160685704640ab
Pratik Rajendra Patil <patilpratik2025@gmail.com>
2025-06-14 16:34:36 +0530

    Update db.js

--- db.js
+++ db.js
@@ -13,7 +13,11 @@ require("dotenv").config();
 // const mongoURL = process.env.MONGODB_URL_LOCAL;
 const mongoURL = process.env.MONGODB_URL;
 //Set up MongoDB connection
-mongoose.connect(mongoURL);
+mongoose.connect(mongoURL,{
+    tls: true,
+  tlsAllowInvalidCertificates: false, // Only true if self-signed
+  ssl: true,
+  sslValidate: true});
 
 //Get the default connection
 //Mongoose maintains a default connection object repesenting the MongoDB connection
